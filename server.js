cd85691c776a07d366887a6bfdad93ff5767ab15
Charles Bartlett <ownageunit@gmail.com>
2022-06-17 11:49:33 -0400

    firebase changes, schema changes

--- server.js
+++ server.js
@@ -4,11 +4,17 @@ const mongoose = require('mongoose');
 const morgan = require('morgan');
 const userSchema = require('./userSchema')
 const Routes = require('./controllers/routeControl');
+const admin = require('firebase-admin')
 
 //--------------------------------------
 const app = express();
 
 require('dotenv').config();
+
+admin.initializeApp({
+  credential: admin.credential.cert(require('./firebase-service-key.json'))
+});
+
 
 //--------------------------------------
 
@@ -30,6 +36,31 @@ app.use(morgan('dev'));
 app.use(express.json());
 // 👆 this creates req.body from incoming JSON request bodies
 app.use('/home', Routes);
+
+// authorization middleware
+app.use( async (req, res, next) => {
+    const token = req.get('Authorization')
+    if (token) {
+        try {
+            const user = await admin.auth().verifyIdToken(token.replace('Bearer ', ''))
+        console.log(user)
+        req.user = user;
+        } catch (error) {
+            req.user = null;
+        }
+    } else {
+        req.user = null;
+    }
+    next();
+})
+
+// lets you know if you're not logged in when making requests
+function isAuthenticated(req, res, next) {
+    if (!req.user) return res.status(401).json({message: 'you must be logged in'})
+    else {
+        return next();
+    }
+}
 
 
 app.listen(PORT, () => {
--- userSchema.js
+++ userSchema.js
@@ -2,17 +2,27 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 
-const User = new Schema ({
-    BingeList: [{
-        titleOfShow: 'string',
-        userRating: 'number',
-        seasons: [
-            {
-                episodes_number: 'number',
-                watched: 'boolean',
-                comments: 'string'
-            }
-        ]
+// const User = new Schema ({
+//     BingeList: [{
+//         titleOfShow: 'string',
+//         userRating: 'number',
+//         seasons: [
+//             {
+//                 episodes_number: 'number',
+//                 watched: 'boolean',
+//                 comments: 'string'
+//             }
+//         ]
 
-    }]
+//     }]
+// })
+
+const tvShowSchema = new Schema ({
+    showTitle: String,
+    userRating: Number,
+    seasons: [],
+    googleID: String,
 })
+
+
+module.exports = mongoose.model('tvShow', tvShowSchema)
