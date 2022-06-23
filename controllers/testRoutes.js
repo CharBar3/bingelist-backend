f9bc655c23e14898a42bce7cafab1a43b29a68eb
Charles Bartlett <ownageunit@gmail.com>
2022-06-23 18:56:43 -0400

    fixed

--- controllers/testRoutes.js
+++ controllers/testRoutes.js
@@ -11,8 +11,8 @@ require('dotenv').config();
 admin.initializeApp({
     credential: admin.credential.cert({
         project_id: process.env.FIREBASE_PROJECT_ID,
-        // private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
-        private_key: process.env.FIREBASE_PRIVATE_KEY,
+        private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+        // private_key: process.env.FIREBASE_PRIVATE_KEY,
         client_email: process.env.FIREBASE_CLIENT_EMAIL,
         token_uri: process.env.FIREBASE_TOKEN_URI
     }),
