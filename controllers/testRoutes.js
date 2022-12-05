76e5e8595c7e032c5ce924b2f10323e3582436cc
Charles Bartlett <ownageunit@gmail.com>
2022-12-04 20:42:24 -0500

    removed commented out firebase service key.json code

--- controllers/testRoutes.js
+++ controllers/testRoutes.js
@@ -1,43 +1,38 @@
-const express = require ('express');
-const admin = require('firebase-admin');
-const tvShow = require('../models/tvShowSchema');
+const express = require("express");
+const admin = require("firebase-admin");
+const tvShow = require("../models/tvShowSchema");
 const router = express.Router();
-require('dotenv').config();
-
-// admin.initializeApp({
-//     credential: admin.credential.cert(require('../firebase-service-key.json'))
-//   });
+require("dotenv").config();
 
 admin.initializeApp({
-    credential: admin.credential.cert({
-        project_id: process.env.FIREBASE_PROJECT_ID,
-        private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
-        // private_key: process.env.FIREBASE_PRIVATE_KEY,
-        client_email: process.env.FIREBASE_CLIENT_EMAIL,
-        token_uri: process.env.FIREBASE_TOKEN_URI
-    }),
-    databaseURL: "https://binge-list-project.herokuapp.com/",
-})
+  credential: admin.credential.cert({
+    project_id: process.env.FIREBASE_PROJECT_ID,
+    private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+    // private_key: process.env.FIREBASE_PRIVATE_KEY,
+    client_email: process.env.FIREBASE_CLIENT_EMAIL,
+    token_uri: process.env.FIREBASE_TOKEN_URI,
+  }),
+  databaseURL: "https://binge-list-project.herokuapp.com/",
+});
 
-
-
-router.use( async (req, res, next) => {
-    const token = req.get('Authorization')
-    if (token) {
-        const user = await admin.auth().verifyIdToken(token.replace('Bearer ', ''))
-        console.log(user)
-        req.user = user;
-    } else {
-        req.user = null;
-    }
-    next();
-})
+router.use(async (req, res, next) => {
+  const token = req.get("Authorization");
+  if (token) {
+    const user = await admin.auth().verifyIdToken(token.replace("Bearer ", ""));
+    console.log(user);
+    req.user = user;
+  } else {
+    req.user = null;
+  }
+  next();
+});
 
 function isAuthenticated(req, res, next) {
-    if (!req.user) return res.status(401).json({message: 'you must be logged in'})
-    else {
-        return next();
-    }
+  if (!req.user)
+    return res.status(401).json({ message: "you must be logged in" });
+  else {
+    return next();
+  }
 }
 
 // example:
@@ -47,52 +42,52 @@ function isAuthenticated(req, res, next) {
 // })
 
 //INDEX--------------------------------
-router.get('/', isAuthenticated, async (req, res) => {
-    try {
-        // grabs google id
-        const googleID = req.user.uid
-        // grabs all data related to google id {googleID}
-        res.json(await tvShow.find({googleID}));
-        //res.json(await tvShow.find({})); we dont need this
-    } catch (error) {
-        console.log('error: ', error);
-        res.json({error: 'somethings wrong go check the console'})
-    }
+router.get("/", isAuthenticated, async (req, res) => {
+  try {
+    // grabs google id
+    const googleID = req.user.uid;
+    // grabs all data related to google id {googleID}
+    res.json(await tvShow.find({ googleID }));
+    //res.json(await tvShow.find({})); we dont need this
+  } catch (error) {
+    console.log("error: ", error);
+    res.json({ error: "somethings wrong go check the console" });
+  }
 });
 
 //DELETE--------------------------------
-router.delete('/:id', isAuthenticated, async (req, res) => {
-    try {
-        res.json(await tvShow.findByIdAndDelete(req.params.id));
-    } catch (error) {
-        console.log('error: ', error);
-        res.json({error: 'something went wrong - check console'});
-    }
+router.delete("/:id", isAuthenticated, async (req, res) => {
+  try {
+    res.json(await tvShow.findByIdAndDelete(req.params.id));
+  } catch (error) {
+    console.log("error: ", error);
+    res.json({ error: "something went wrong - check console" });
+  }
 });
 // UPDATE--------------------------------
-router.put('/:id', isAuthenticated, async (req, res) => {
-    try {
-        res.json(await tvShow.findByIdAndUpdate(req.params.id, req.body, { new: true } ));
-                                            // Just in case you forgot what ^^^ it sends back the updated version so we aren't
-                                            ///stuck with the old one.
-
-    } catch (error) {
-        console.log('error: ', error);
-        res.json({error: 'something went wrong - check console'});
-    }
+router.put("/:id", isAuthenticated, async (req, res) => {
+  try {
+    res.json(
+      await tvShow.findByIdAndUpdate(req.params.id, req.body, { new: true })
+    );
+    // Just in case you forgot what ^^^ it sends back the updated version so we aren't
+    ///stuck with the old one.
+  } catch (error) {
+    console.log("error: ", error);
+    res.json({ error: "something went wrong - check console" });
+  }
 });
 
 //CREATE--------------------------------
-router.post('/', isAuthenticated, async (req, res) => {
-    try {
-        // need this to add google id to the body when adding a bingelist
-        req.body.googleID = req.user.uid
-        res.json(await tvShow.create(req.body));
-    } catch (error) {
-        console.log('error: ', error);
-        res.json({error: 'something went wrong - check console'});
-    }
+router.post("/", isAuthenticated, async (req, res) => {
+  try {
+    // need this to add google id to the body when adding a bingelist
+    req.body.googleID = req.user.uid;
+    res.json(await tvShow.create(req.body));
+  } catch (error) {
+    console.log("error: ", error);
+    res.json({ error: "something went wrong - check console" });
+  }
 });
-
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,42 +1,37 @@
-const express = require ('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
-const morgan = require('morgan');
-const TvShow = require('./models/tvShowSchema')
-const Routes = require('./controllers/routeControl');
-const testRoute = require('./controllers/testRoutes')
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const TvShow = require("./models/tvShowSchema");
+const Routes = require("./controllers/routeControl");
+const testRoute = require("./controllers/testRoutes");
 // const admin = require('firebase-admin')
 
 //--------------------------------------
 const app = express();
 
-require('dotenv').config();
-
-// admin.initializeApp({
-//   credential: admin.credential.cert(require('./firebase-service-key.json'))
-// });
-
+require("dotenv").config();
 
 //--------------------------------------
 
-const { PORT, MONGODB_URL} = process.env;
+const { PORT, MONGODB_URL } = process.env;
 
 //Connect to mongoDB
 mongoose.connect(MONGODB_URL);
 
 // Mongo Status Listeners
 mongoose.connection
-.on('connected', () => console.log('Connected to MongoDB'))
-.on('error', (err) => console.log('Error with MongoDB: ' + err.message))
+  .on("connected", () => console.log("Connected to MongoDB"))
+  .on("error", (err) => console.log("Error with MongoDB: " + err.message));
 
 // const tvData = mongoose.model('tvData', tvShowSchema);
 
 //MIDDLEWARE----------------------------
 app.use(cors());
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(express.json());
 // 👆 this creates req.body from incoming JSON request bodies
-app.use('/bingelist', testRoute);
+app.use("/bingelist", testRoute);
 
 // // authorization middleware
 // app.use( async (req, res, next) => {
@@ -74,11 +69,10 @@ app.use('/bingelist', testRoute);
 //         res.redirect('/bingelist')
 //     })
 // })
-app.get('/', (req, res) => {
-    res.redirect('/bingelist')
-})
-
+app.get("/", (req, res) => {
+  res.redirect("/bingelist");
+});
 
 app.listen(PORT || 4000, () => {
-    console.log(`Port: ${PORT} is now active`);
+  console.log(`Port: ${PORT} is now active`);
 });
